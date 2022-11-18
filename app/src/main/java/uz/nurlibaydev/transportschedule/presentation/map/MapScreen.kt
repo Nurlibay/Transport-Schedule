1541c49b01244741105a5a4c67e7524efa2265cc
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-11-18 12:28:14 +0500

    map added

--- app/src/main/java/uz/nurlibaydev/transportschedule/presentation/map/MapScreen.kt
+++ app/src/main/java/uz/nurlibaydev/transportschedule/presentation/map/MapScreen.kt
@@ -1,5 +1,8 @@
 package uz.nurlibaydev.transportschedule.presentation.map
 
+import androidx.fragment.app.Fragment
+import uz.nurlibaydev.transportschedule.R
+
 // Created by Jamshid Isoqov an 11/18/2022
-class MapScreen {
+class MapScreen : Fragment(R.layout.screen_map) {
 }
