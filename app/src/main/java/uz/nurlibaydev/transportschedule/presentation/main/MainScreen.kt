9dbaef36663cd4fbc1773aba7ff0cbe0c3a92841
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-11-18 12:12:03 +0500

    main screen added

---
+++ app/src/main/java/uz/nurlibaydev/transportschedule/presentation/main/MainScreen.kt
@@ -0,0 +1,8 @@
+package uz.nurlibaydev.transportschedule.presentation.main
+
+import androidx.fragment.app.Fragment
+import uz.nurlibaydev.transportschedule.R
+
+// Created by Jamshid Isoqov an 11/18/2022
+class MainScreen : Fragment(R.layout.screen_main) {
+}
---
+++ app/src/main/java/uz/nurlibaydev/transportschedule/presentation/map/MapScreen.kt
@@ -0,0 +1,5 @@
+package uz.nurlibaydev.transportschedule.presentation.map
+
+// Created by Jamshid Isoqov an 11/18/2022
+class MapScreen {
+}
